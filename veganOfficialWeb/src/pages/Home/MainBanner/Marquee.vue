674355fe310362c3eb9a523c909a20c2a2d8bf95
th0ma5C <thomas29111@gmail.com>
2024-02-01 22:50:17 +0800

    0201vegan

--- veganOfficialWeb/src/pages/Home/MainBanner/Marquee.vue
+++ veganOfficialWeb/src/pages/Home/MainBanner/Marquee.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="container">
         <div class="marquee">
-            <button class="btn-prev" @click="prev">&lt;</button>
-            <button class="btn-next" @click="next">&gt;</button>
-            <div class="swiper"
-                :style="{ transform: `translateX(${left}px)` }"
-                ref="slide">
-                <p v-for="(item, index) in swiper" :key="index">
+            <button class="btn-prev"
+                @click="prev()">&lt;</button>
+            <button class="btn-next"
+                @click="next()">&gt;</button>
+            <div class="swiper" ref="slide"
+                :style="{ transform: `translateX(${left}px)` }">
+                <p v-for="(item) in swiper" :key="item.title">
                     {{ item.title }} </p>
             </div>
         </div>
@@ -16,25 +17,34 @@
 <script lang="ts" setup>
 import { ref, computed } from 'vue'
 
-const swiper = [
+const swiper = ref([
     { title: '最新美味上架！立即探索我們最新的素食餐盒和果昔，品嚐獨特的素食美味。' },
     { title: '最新消息、特別優惠、限量商品和精彩活動都在這裡！。' },
     { title: '過敏原報告，查看我們食物過敏警報，確保您點的安心。' },
     { title: 'APP限定好康：下載我們的APP，即刻獲得專屬優惠及最新活動資訊。' },
     { title: '加入會員，享專屬優待！加入我們的會員計畫，即刻享有限定優惠和會員專屬好康。' },
-]
+])
 
 let slide = ref()
 let n = ref(0)
-let totalSlides = swiper.length
+let totalSlides = swiper.value.length
 let left = computed(() => -(slide.value?.offsetWidth || 0) * n.value)
 
 function next() {
     if (n.value < (totalSlides - 1)) {
         n.value++;
     } else {
-        n.value = 0;
+        n.value = 0
     }
+    // console.log(n.value, swiper.value);
+    // if (n > 0) {
+    //     const shiftItem = swiper.value.shift();
+    //     swiper.value.push(shiftItem);
+    //     return
+    // } else {
+    //     const shiftItem = swiper.value.pop();
+    //     swiper.value.unshift(shiftItem);
+    // }
 }
 function prev() {
     if (n.value > 0) {
@@ -79,7 +89,7 @@ function prev() {
 
         .swiper {
             display: flex;
-            transition: transform 0.5s ease;
+            transition: all 0.5s ease;
 
             p {
                 text-align: center;
