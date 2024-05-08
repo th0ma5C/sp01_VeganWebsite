996e26536db095c4ddfdcd5c2d305205d99480d2
th0ma5C <thomas29111@gmail.com>
2024-05-08 22:05:18 +0800

    0508vegan

--- veganOfficialWeb/src/pages/Home/Concept/Concept.vue
+++ veganOfficialWeb/src/pages/Home/Concept/Concept.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>
-        <div>
-            <img src="" alt=""
-                style="height: 450px; width: 600px;">
-            <div>
+    <div class="container">
+        <section>
+            <div class="textClip">NEWS</div>
+            <div class="text">
                 <h3>理念1</h3>
                 <p>111</p>
             </div>
-        </div>
-        <div>
-            <img src="" alt=""
-                style="height: 450px; width: 600px;">
-            <div>
+        </section>
+        <section>
+            <div class="textClip">About Us</div>
+            <div class="text">
                 <h3>理念2</h3>
                 <p>222</p>
             </div>
-        </div>
+        </section>
     </div>
 </template>
 
@@ -23,4 +21,48 @@
 
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@font-face {
+    font-family: 'MrDafoe';
+    src: url('@assets/fonts/Mr_Dafoe/MrDafoe-Regular.woff2') format('woff2');
+}
+
+.container {
+    @include main-part;
+    @include flex-center-center;
+    gap: 24px;
+
+    section {
+        @include WnH(612px, 408px);
+        background: linear-gradient(152deg,
+                hsla(72, 67%, 57%, 0.15) 25%, hsla(9, 94%, 45%, 0.15) 80%);
+
+        .textClip {
+            @include WnH(100%);
+            background: url('@assets/img/Home/Concept/fruit.jpg') center / cover no-repeat;
+            background-clip: text;
+            font-family: 'MrDafoe', 'NotoSans';
+            color: transparent;
+            font-size: 150px;
+            text-align: center;
+            line-height: 408px;
+            transition: all 0.5s ease;
+        }
+
+        &:hover {
+            .textClip {
+                font-size: 175px;
+            }
+        }
+
+        .text {
+            background-color: $primeBacColor;
+
+            &::after {
+                content: '';
+            }
+        }
+    }
+
+}
+</style>
