da621f53f19e0dea73fdf4ddd43da7f328aa53dd
th0ma5C <thomas29111@gmail.com>
2024-06-24 22:08:08 +0800

    0624 concept

--- veganOfficialWeb/src/pages/Home/Concept/Concept.vue
+++ veganOfficialWeb/src/pages/Home/Concept/Concept.vue
@@ -7,17 +7,13 @@
                     最新消息
                 </h2>
                 <ul>
-                    <li @click="newsList.changeTab($event)">
-                        最新
-                    </li>
-                    <li @click="newsList.changeTab($event)">
-                        活動
-                    </li>
-                    <li @click="newsList.changeTab($event)">
-                        優惠
-                    </li>
-                    <li @click="newsList.changeTab($event)">
-                        會員
+                    <li v-for="(item, index) in newsList.tabs"
+                        :key="index"
+                        :class="{ hover: hoverItem == index || currItem == index }"
+                        @mouseover="setHover(index)"
+                        @mouseout="setHover(null)"
+                        @click="newsList.changeTab(index, $event)">
+                        {{ item }}
                     </li>
                 </ul>
             </div>
@@ -27,7 +23,10 @@
                     v-show="tab == newsList.tab">
                     <ul>
                         <li v-for="(news) in newsList.showNews"
-                            :key="news._id">
+                            :key="news._id"
+                            @mouseover="setHover(news._id!)"
+                            @mouseout="setHover(null)">
+                            <!-- <div class="listWrapper"> -->
                             <div class="date">
                                 {{ news.date }}
                             </div>
@@ -35,8 +34,15 @@
                                 {{ news.label }}
                             </div>
                             <div>
-                                {{ news.title }}
+                                <span style="opacity: 1;"
+                                    :class="{
+                                        hover: hoverItem == news._id,
+                                        hideSlide: isHover
+                                    }">
+                                    {{ news.title }}
+                                </span>
                             </div>
+                            <!-- </div> -->
                         </li>
                     </ul>
                 </div>
@@ -51,9 +57,12 @@
         </div>
         <transition name="marquee">
             <div class="marquee" v-show="enter">
-                <span ref="test">Information / News</span>
-                <span>Information / News</span>
-                <span>Information / News</span>
+                <span>
+                    Information / News &nbsp;&nbsp;&nbsp;
+                </span>
+                <span class="cloneSpan">
+                    Information / News &nbsp;&nbsp;&nbsp;
+                </span>
             </div>
         </transition>
     </div>
@@ -69,12 +78,13 @@ import { useNewsStore } from '@/store/newsStore';
 import type { Ref, ComputedRef } from 'vue';
 import moment from 'moment';
 
-//TODO: 背景跑馬燈、hover高亮圓圈、底線畫出
+//TODO: hover高亮圓圈
 /**
  * //新聞資料建置、請求資料編寫、換頁邏輯
- * *list比例調整
+ * //list比例調整
  * //用observer 控制跑馬燈是否顯示
- * *marquee動畫
+ * //marquee動畫
+ * //底線劃出
  * *btn hover效果、箭頭SVG
  * *內文靠上
  * *news pinia 重寫 https://medium.com/@lovebuizel/vue3-pinia-%E4%B8%AD%E5%A6%82%E4%BD%95%E5%84%AA%E9%9B%85%E7%9A%84%E4%BD%BF%E7%94%A8api-5e2636691d8b
@@ -96,9 +106,12 @@ let newsList = reactive({
     tabs: ['最新', '活動', '優惠', '會員'],
     tab: '最新',
     showNews: [] as newsItem[],
-    changeTab(e: MouseEvent | null) {
+    changeTab(index: number, e: MouseEvent | null) {
         const target = e?.target as HTMLElement
         this.tab = target.innerHTML.trim();
+        currItem.value = index;
+        isHover.value = true;
+        setTimeout(() => (isHover.value = false), 300)
     }
 })
 
@@ -116,7 +129,7 @@ watchEffect(() => {
     })
 });
 
-// 背景顯示
+// 背景跑馬燈顯示
 let tabContainer = ref()
 let enter = ref(false)
 
@@ -126,7 +139,6 @@ let showBac = (entries: IntersectionObserverEntry[] | undefined) => {
             enter.value = entry.isIntersecting;
         })
     }
-
 }
 
 const observer = new IntersectionObserver(showBac, {
@@ -134,6 +146,15 @@ const observer = new IntersectionObserver(showBac, {
     rootMargin: '-460px 0px',
     threshold: [0]
 });
+
+// 底線動畫
+let isHover = ref(false); //控制換頁時不顯示動畫
+let hoverItem: Ref<number | string | null> = ref(0);
+let currItem = ref(0);
+
+function setHover(index: number | string | null) {
+    hoverItem.value = index;
+}
 
 onMounted(() => {
     observer.observe(tabContainer.value);
@@ -149,6 +170,26 @@ onUnmounted(() => {
     // border: 1px solid;
 }
 
+@keyframes marquee {
+    from {
+        transform: translateX(100%)
+    }
+
+    to {
+        transform: translateX(-100%)
+    }
+}
+
+@keyframes cloneMarquee {
+    from {
+        transform: translateX(0)
+    }
+
+    to {
+        transform: translateX(-200%)
+    }
+}
+
 .newsContainer {
     @include main-part;
     @include flex-center-center;
@@ -157,7 +198,7 @@ onUnmounted(() => {
     margin-top: 96px;
     height: 888px;
     position: relative;
-    padding: 0 6rem;
+    // padding: 0 6rem;
     overflow: hidden;
 
     .marquee {
@@ -173,6 +214,7 @@ onUnmounted(() => {
         z-index: 0;
 
         span {
+            animation: marquee 450s -225s linear infinite;
             background: linear-gradient(0deg, transparent 25%, black 50%);
             background-clip: text;
             color: transparent;
@@ -180,8 +222,12 @@ onUnmounted(() => {
             font-optical-sizing: auto;
             font-weight: 400;
             font-style: normal;
-            font-size: 12.8vw;
+            font-size: 11.62vw;
             white-space: nowrap;
+        }
+
+        .cloneSpan {
+            animation: cloneMarquee 450s linear infinite;
         }
     }
 
@@ -213,7 +259,7 @@ onUnmounted(() => {
         z-index: 1;
 
         &>div {
-            width: 80%;
+            width: 100%;
         }
     }
 
@@ -223,7 +269,7 @@ onUnmounted(() => {
         border: 1px solid rgba(0, 0, 0, 0.5);
         border-radius: 40px;
         font-size: 20px;
-        margin: 2rem 10% 2rem auto;
+        margin: 2rem 15% 2rem auto;
 
         button {
             @include flex-center-center;
@@ -238,6 +284,7 @@ onUnmounted(() => {
     align-items: center;
     justify-content: space-between;
     margin-top: 3rem;
+    padding: 0 calc(10% + 6rem);
 
     h2 {
         display: inline-flex;
@@ -257,11 +304,63 @@ onUnmounted(() => {
         display: flex;
 
         li {
+            @extend %pseudoLine;
+
             cursor: pointer;
             font-size: 1.5rem;
             margin-left: 2rem;
             user-select: none;
+            position: relative;
+            overflow: hidden;
+            transition: opacity 0.3s ease;
         }
+    }
+}
+
+// hover底線動畫
+%pseudoLine {
+    &::after {
+        @include WnH(100%, 1px);
+        animation: lineOut 0.3s ease forwards;
+        background-color: black;
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        transform: translateX(-100%);
+        transition: transform 0.3s ease;
+    }
+
+    &.hover::after {
+        animation: lineIn 0.3s ease forwards;
+    }
+
+    &:not(.hover) {
+        opacity: 0.5;
+    }
+
+    &.hideSlide::after {
+        opacity: 0;
+    }
+}
+
+@keyframes lineIn {
+    from {
+        transform: translateX(-100%);
+    }
+
+    to {
+        transform: translateX(0%);
+    }
+}
+
+@keyframes lineOut {
+    from {
+        transform: translateX(0%);
+    }
+
+    to {
+        transform: translateX(100%);
     }
 }
 
@@ -272,7 +371,7 @@ onUnmounted(() => {
 .tab {
     display: flex;
     height: 100%;
-    padding: 0 4rem;
+    // padding: 0 4rem;
 
     ul {
         // height: 100%;
@@ -289,7 +388,7 @@ onUnmounted(() => {
         content: '';
         position: absolute;
         left: 0;
-        transform: translateX(calc(-1 * (10rem + (100vw - 6rem) * 0.1)));
+        // transform: translateX(calc(-1 * (10rem + (100vw - 6rem) * 0.1)));
     }
 
     li {
@@ -298,6 +397,7 @@ onUnmounted(() => {
         display: flex;
         flex: 0 0 20%;
         position: relative;
+        padding: 0 calc(12% + 6rem);
 
         &::before {
             @extend %pseudo-li-line;
@@ -307,6 +407,10 @@ onUnmounted(() => {
         &:last-child::after {
             @extend %pseudo-li-line;
             bottom: 0;
+        }
+
+        .listWrapper {
+            // padding: 0 10%;
         }
 
         .date {
@@ -329,6 +433,15 @@ onUnmounted(() => {
             flex: 8;
             padding-left: 4rem;
             font-size: 20px;
+
+            span {
+                @extend %pseudoLine;
+
+                cursor: pointer;
+                display: inline-block;
+                position: relative;
+                overflow: hidden;
+            }
         }
     }
 }
